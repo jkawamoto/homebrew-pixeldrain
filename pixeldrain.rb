84e11c5b46cce90d626b2e6bedfb12632c5e9c28
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2019-02-24 19:12:14 -0500

    v0.3.1

--- pixeldrain.rb
+++ pixeldrain.rb
@@ -2,18 +2,18 @@ require 'rbconfig'
 class Pixeldrain < Formula
   desc "Pixeldrain client"
   homepage "https://github.com/jkawamoto/go-pixeldrain"
-  version "0.3.0"
+  version "0.3.1"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG['host_os']
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.3.0/pd_v0.3.0_darwin_amd64.zip"
-      sha256 "c31a45d8279b50df997f65a44c2c1d89f4bc7c13488339c817c4696b8bd159b7"
+      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.3.1/pd_v0.3.1_darwin_amd64.zip"
+      sha256 "16f31c4eb60fc6b8f6f17c3850af90420941b7d0e0e94d9f9bb3f1b2ab74aa01"
     when /linux/
-      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.3.0/pd_v0.3.0_linux_amd64.tar.gz"
-      sha256 "14c055c33293bef397c359e246185a15ad5b6bc08ecd00b3ca80a45767748efa"
+      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.3.1/pd_v0.3.1_linux_amd64.tar.gz"
+      sha256 "2e991bea1f61847c95a22bdc90cfca3a0f212b98854fbed93f428bd1264866bc"
     when /solaris|bsd/
       :unix
     else
@@ -24,11 +24,11 @@ class Pixeldrain < Formula
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.3.0/pd_v0.3.0_darwin_386.zip"
-      sha256 "4af631263fb0a9b547aee3d9dee4ad1526354bf2a18fd43ab326b57a5a0055e6"
+      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.3.1/pd_v0.3.1_darwin_386.zip"
+      sha256 "38f7fa0a24ba8cf1ea3ed24f0a99a7b5eefc9fca45eb9213438d99ad6b535c7b"
     when /linux/
-      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.3.0/pd_v0.3.0_linux_386.tar.gz"
-      sha256 "860158d445938a17e7308c614df07473b20b0c789fad9c690912a2386327295c"
+      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.3.1/pd_v0.3.1_linux_386.tar.gz"
+      sha256 "55e6c2b62a4c721e2403d8da1c6ec2e65e0f92bc99a8fe744aad0afbb0af459f"
     when /solaris|bsd/
       :unix
     else
