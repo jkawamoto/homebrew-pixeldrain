47668abd02d6e6fd17f6308b14fd79c7d749163b
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2023-04-08 02:08:45 -0400

    Update pixeldrain.rb

--- pixeldrain.rb
+++ pixeldrain.rb
@@ -10,16 +10,16 @@ class Pixeldrain < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.6.3/pd_0.6.3_darwin_arm64.tar.gz"
-      sha256 "01152fe8449fb1544810d76bb07f3d9c7381b01f9ed57e8f6f5b7686a1a8764f"
+      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.7.0/pd_0.7.0_darwin_arm64.tar.gz"
+      sha256 "ad1fc0faeb655a23c905bf17ce6745eb4955193f4d0d22a53563342add84090e"
 
       def install
         bin.install "pd"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.6.3/pd_0.6.3_darwin_amd64.tar.gz"
-      sha256 "738ebf193d07bf24d74cd2ae29658860fb2d6e69d3e6e8ba028e1f6ac91ab18e"
+      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.7.0/pd_0.7.0_darwin_amd64.tar.gz"
+      sha256 "7cbbd97c510f390d109ea11af74e3b57330c30ec0877f2acdff337e612e347c4"
 
       def install
         bin.install "pd"
@@ -29,16 +29,16 @@ class Pixeldrain < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.6.3/pd_0.6.3_linux_arm64.tar.gz"
-      sha256 "35fe331331699e8d23b8e1b6afcef71c656e973be374152a8880507c10670327"
+      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.7.0/pd_0.7.0_linux_arm64.tar.gz"
+      sha256 "a98f114bfcdd2df341421168d81c42a99558a5a61f9ce29e4f1510147f493815"
 
       def install
         bin.install "pd"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.6.3/pd_0.6.3_linux_amd64.tar.gz"
-      sha256 "5d095bab9180d259edb14c1dbf2e3afc38db2ec531f7818c3f5351e09b8efcd6"
+      url "https://github.com/jkawamoto/go-pixeldrain/releases/download/v0.7.0/pd_0.7.0_linux_amd64.tar.gz"
+      sha256 "a8bc76de18622f6289367f8dd7ca8e2be7274af45dec874ac485dec8f8d87485"
 
       def install
         bin.install "pd"
